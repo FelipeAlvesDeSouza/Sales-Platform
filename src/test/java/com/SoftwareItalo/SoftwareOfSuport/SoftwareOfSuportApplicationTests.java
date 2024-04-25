4528912b5485b94834f82ecf6f1a8ee9f6dce25e
Felipe Alves <felipealvess_hpg_@hotmail.com>
2024-04-25 13:53:28 -0300

    Criação de packages

---
+++ src/test/java/com/SoftwareItalo/SoftwareOfSuport/SoftwareOfSuportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoftwareItalo.SoftwareOfSuport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareOfSuportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
